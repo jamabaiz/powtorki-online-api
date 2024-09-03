f0da95d71eaa9185dbe3a0a04d5d5eb34098458b
Jan Bartula <jan.bartula@allmedica.pl>
2024-09-03 22:02:56 +0200

    no admin

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -22,81 +22,81 @@ page_type_to_model = {
     PageTypes.QAPage: models.QAPage,
 }
 
-
-@router.post("/import_from_file/", dependencies=[Permission("view", [(Allow, Admin, All)])])
-def import_from_file(files: list[UploadFile], taxonomy: int = Form(), page_type: int = Form(),
-                     db: Session = Depends(get_db)):
-    new_pages = []
-
-    tax = db.query(models.ChapterTaxonomy).filter(models.ChapterTaxonomy.id == taxonomy).first()
-
-    if page_type in [PageTypes.DocumentPage, PageTypes.ScriptPage, PageTypes]:
-        new_pages += process_documents(page_type_to_model[page_type], files)
-    elif page_type in [PageTypes.MindmapPage]:
-        new_pages += process_pdf(page_type_to_model[page_type], files)
-    elif page_type in [PageTypes.QAPage]:
-        for file in files:
-            qa_tax = models.Taxonomy(name=file.filename, id_parent=tax.id)
-            db.add(qa_tax)
-            for quiz_page in process_qa(file):
-                map_page_tax = models.MapPageTaxonomy()
-                map_page_tax.taxonomy = qa_tax
-                quiz_page.taxonomies.append(map_page_tax)
-                db.add(map_page_tax)
-                db.add(quiz_page)
-    elif page_type in [PageTypes.CalendarPage]:
-        for file in files:
-            new_pages += process_dates(file, db)
-    elif page_type in [PageTypes.CharacterPage]:
-        for file in files:
-            new_pages += process_characters(file)
-    elif page_type in [PageTypes.DictionaryPage]:
-        for file in files:
-            new_pages += process_dictionary(file)
-    elif page_type in [PageTypes.QuizPage]:
-        for file in files:
-            process_quiz(file, db, tax)
-    else:
-        raise Exception(f"Unhandled typeId: {page_type}")
-
-    for page in new_pages:
-        map_page_tax = models.MapPageTaxonomy()
-        map_page_tax.taxonomy = tax
-        page.taxonomies.append(map_page_tax)
-        db.add(page)
-        db.add(map_page_tax)
-
-    print(f"Flushing")
-    db.flush()
-
-    return {"filenames": (file.filename for file in files), "taxonomy": taxonomy, "page_type": page_type}
-
-
-@router.get("/admin/", dependencies=[Permission("view", [(Allow, Admin, All)])])
-async def main():
-    content = """
-<!DOCTYPE html>
-<html>
-<body>
-<form action="/import_from_file/" enctype="multipart/form-data" method="post">
-<label>Files: <input name="files" type="file" multiple></label><br>
-<label>Type: <select name="page_type" required>
-  <option value="2">Materiały do nauki</option>
-  <option value="3">Lekcje</option>
-  <option value="4">Postacie</option>
-  <option value="5">Kalendarz</option>
-  <option value="6">Pojęcia</option>
-  <option value="7">Pytania i odpowiedzi</option>
-  <option value="8">Quiz</option>
-  <option value="9">Mapa mysli</option>
-</select></label><br>
-<label>Taxonomy: <select name="taxonomy" required>
-  <option value="228">XVIII Wiek - Historia Polski</option>
-  <option value="229">XVIII Wiek - Historia Powszechna</option>
-</select></label></br>
-<input type="submit">
-</form>
-</body>
-</html>
-    """
-    return HTMLResponse(content=content)
+#
+# @router.post("/import_from_file/", dependencies=[Permission("view", [(Allow, Admin, All)])])
+# def import_from_file(files: list[UploadFile], taxonomy: int = Form(), page_type: int = Form(),
+#                      db: Session = Depends(get_db)):
+#     new_pages = []
+#
+#     tax = db.query(models.ChapterTaxonomy).filter(models.ChapterTaxonomy.id == taxonomy).first()
+#
+#     if page_type in [PageTypes.DocumentPage, PageTypes.ScriptPage, PageTypes]:
+#         new_pages += process_documents(page_type_to_model[page_type], files)
+#     elif page_type in [PageTypes.MindmapPage]:
+#         new_pages += process_pdf(page_type_to_model[page_type], files)
+#     elif page_type in [PageTypes.QAPage]:
+#         for file in files:
+#             qa_tax = models.Taxonomy(name=file.filename, id_parent=tax.id)
+#             db.add(qa_tax)
+#             for quiz_page in process_qa(file):
+#                 map_page_tax = models.MapPageTaxonomy()
+#                 map_page_tax.taxonomy = qa_tax
+#                 quiz_page.taxonomies.append(map_page_tax)
+#                 db.add(map_page_tax)
+#                 db.add(quiz_page)
+#     elif page_type in [PageTypes.CalendarPage]:
+#         for file in files:
+#             new_pages += process_dates(file, db)
+#     elif page_type in [PageTypes.CharacterPage]:
+#         for file in files:
+#             new_pages += process_characters(file)
+#     elif page_type in [PageTypes.DictionaryPage]:
+#         for file in files:
+#             new_pages += process_dictionary(file)
+#     elif page_type in [PageTypes.QuizPage]:
+#         for file in files:
+#             process_quiz(file, db, tax)
+#     else:
+#         raise Exception(f"Unhandled typeId: {page_type}")
+#
+#     for page in new_pages:
+#         map_page_tax = models.MapPageTaxonomy()
+#         map_page_tax.taxonomy = tax
+#         page.taxonomies.append(map_page_tax)
+#         db.add(page)
+#         db.add(map_page_tax)
+#
+#     print(f"Flushing")
+#     db.flush()
+#
+#     return {"filenames": (file.filename for file in files), "taxonomy": taxonomy, "page_type": page_type}
+#
+#
+# @router.get("/admin/", dependencies=[Permission("view", [(Allow, Admin, All)])])
+# async def main():
+#     content = """
+# <!DOCTYPE html>
+# <html>
+# <body>
+# <form action="/import_from_file/" enctype="multipart/form-data" method="post">
+# <label>Files: <input name="files" type="file" multiple></label><br>
+# <label>Type: <select name="page_type" required>
+#   <option value="2">Materiały do nauki</option>
+#   <option value="3">Lekcje</option>
+#   <option value="4">Postacie</option>
+#   <option value="5">Kalendarz</option>
+#   <option value="6">Pojęcia</option>
+#   <option value="7">Pytania i odpowiedzi</option>
+#   <option value="8">Quiz</option>
+#   <option value="9">Mapa mysli</option>
+# </select></label><br>
+# <label>Taxonomy: <select name="taxonomy" required>
+#   <option value="228">XVIII Wiek - Historia Polski</option>
+#   <option value="229">XVIII Wiek - Historia Powszechna</option>
+# </select></label></br>
+# <input type="submit">
+# </form>
+# </body>
+# </html>
+#     """
+#     return HTMLResponse(content=content)
